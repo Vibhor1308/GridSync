5159f2950f2bd311fb24d11e76c51b9cfabaeb06
tyagivibhor0508@gmail.com <tyagivibhor0508@gmail.com>
2026-05-31 19:16:10 +0530

    Initial GridSync setup.

--- app/src/main/java/com/example/GridSync/MainActivity.kt
+++ app/src/main/java/com/example/GridSync/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.GridSync.ui.theme.GenSyncTheme
+import com.example.GridSync.ui.theme.GridSyncTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GenSyncTheme {
+            GridSyncTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GenSyncTheme {
+    GridSyncTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/GridSync/ui/theme/Theme.kt
+++ app/src/main/java/com/example/GridSync/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun GenSyncTheme(
+fun GridSyncTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
